311540c5bea4bd53d2243d2f49b65a6cfb5ff857
unknown <fede_andrijasevich@hotmail.com>
2021-03-20 20:07:20 -0300

    Se termino index.php

--- header.php
+++ header.php
@@ -6,9 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/headeryfooter.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
     <header>
         <section id="header">
             <div id="logoHeader">
@@ -23,8 +29,8 @@
         </section>
 
         <nav>
-            <a href="index.php">INICIO</a>
-            <a href="contacto.php">CONTACTO</a>
+            <a href="index.php" style="text-decoration: none;">INICIO</a>
+            <a href="contacto.php" style="text-decoration: none;">CONTACTO</a>
         </nav>
     </header>
 </body>
--- index.php
+++ index.php
@@ -9,12 +9,61 @@
     <title>Estudio Guiguet</title>
 </head>
 <body>
+
     <?php include "header.php"?>
     
     <main>
         <section>
             <img src="img/Slider.png" alt="" id="slider">
         </section>    
+        
+        <section >
+            <h3 class="text-center" id="nuestrosServicios">NUESTROS SERVICIOS</h3>
+            <div id="resumen">
+                <div class="card imgResumen" style="width: 18rem; width: 350px; border: solid 2px" id="ResponsablesInscriptos">
+                    <div class="card-header text-center titulo-tarjeta" style="background-color: #bdc3c7; height: 70px; border-bottom: solid 2px">
+                        Responsables Inscriptos
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Inscripción y asesoramiento</li>
+                        <li class="list-group-item">Liquidación y presentación de
+                            Declaraciones Juradas de
+                            impuestos mensuales (IVA,
+                            Ingresos Brutos de contribuyentes
+                            locales y de Convenio Multilateral</li>
+                        <li class="list-group-item">Liquidación y presentación de
+                            Declaraciones Juradas de
+                            impuestos anuales (Ganancias,
+                            Bienes Personales, Renta
+                            Financiera-Impuesto Cedular,
+                            Participaciones Societarias)</li>
+                        <li class="list-group-item">Confección y presentación de
+                            Declaraciones Juradas anuales de
+                            Ingresos Brutos.</li>
+                    </ul>
+                </div>
+
+                <div class="card" style="width: 18rem; width: 350px; border: solid 2px">
+                    <div class="card-header text-center titulo-tarjeta" style="background-color: #bdc3c7;height: 70px; border-bottom: solid 2px">
+                        Régimen Simplificado - MONOTRIBUTO
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Inscripción y asesoramiento</li>
+                        <li class="list-group-item">Monitores permanente del nivel de
+                            facturación</li>
+                        <li class="list-group-item">Recategorizaciones</li>
+                        <li class="list-group-item">Liquidación y presentación de
+                            Declaraciones Juradas de Ingresos
+                            Brutos contribuyentes locales y
+                            Convenio Multilateral</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="text-center" id="contenedor-boton">
+                <a href="contacto.php" class="btn text-center" id="boton">CONTACTANOS</a>
+            </div>
+
+        </section>
     </main>
     
     <?php include "footer.php"?>
